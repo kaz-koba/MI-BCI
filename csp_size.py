0541a5882d1e3d77ecb0ffcaf74175470455c370
shaream <sprinter0208@outlook.jp>
2020-04-18 16:03:44 +0900

    2020/04/18

---
+++ csp_size.py
@@ -0,0 +1,101 @@
+import configparser
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import optuna
+
+from sklearn.svm import SVC
+from sklearn.model_selection import ShuffleSplit, cross_val_score, StratifiedKFold
+
+import mne
+from mne import concatenate_epochs
+
+from mne.decoding import CSP , Vectorizer
+from sklearn import preprocessing
+from sklearn.metrics import confusion_matrix, classification_report
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from pathfile import PATHfile
+from epoch_raw import Epoch_raw
+import pickle_make
+
+def objective(trial):
+    C = trial.suggest_loguniform('C', 1e-4, 1e4)
+    gamma = trial.suggest_loguniform('gamma', 1e-4, 1e4)
+
+    svm = SVC(C=C, gamma = gamma, kernel='rbf', cache_size=100)
+
+    scores = cross_val_score(svm, data40, labels, cv=cv, n_jobs=1)
+
+    print("Classification accuracy: {}" .format(np.mean(scores)))
+
+    return np.mean(scores)
+
+# set epoching parameters
+tmin, tmax =-1., 4.
+inifile = configparser.ConfigParser()
+inifile.read('./parameter.ini', 'UTF-8')
+
+day = inifile.get('setting', 'day')
+name = inifile.get('setting', 'name')
+trial = inifile.get('setting', 'trial')
+task_num = inifile.get('setting', 'task_num')
+path = inifile.get('setting', 'path')
+
+
+if path == "day":
+    path_b = [(PATHfile.edfpath(name, day, "1"), PATHfile.eventpath(name, day, "1")),
+        (PATHfile.edfpath(name, day, "2"), PATHfile.eventpath(name, day, "2"))]
+        (PATHfile.edfpath(name, day, "3"), PATHfile.eventpath(name, day, "3"))]
+elif path == "trial":
+    path_b = [(PATHfile.edfpath(name, day, trial), PATHfile.eventpath(name, day, trial))]
+
+if task_num == "2":
+    event_id = dict(Left=1, Right=2)  # map event IDs to tasks
+    target_names = ['left', 'right']
+    data40 = np.empty((len(path_b)*40,0))
+elif task_num == "3":
+    event_id = dict(Left=1, Right=2, Another=3)
+    target_names = ['left', 'right', 'Another']
+    data40 = np.empty((len(path_b)*60,0))
+
+
+#frequency bands
+iter_freqs = [
+    ('Theta', 4, 7, 1.0),
+    ('Alpha', 8, 12, 1.0),
+    ('Beta', 13, 25, 1.0),
+    ('Gamma', 30, 45, 1.0)
+]
+
+#parameters
+svm = SVC(C=float(inifile.get('setting', 'C')), gamma = float(inifile.get('setting', 'gamma')), kernel='rbf', 
+        cache_size=100)
+cv = ShuffleSplit(10, test_size=0.2, random_state=42)
+
+# set epoching parameters
+tmin, tmax =-1., 4.
+
+csp_map = list()
+for band, fmin, fmax, mag in iter_freqs:
+    epochs = []
+    scaler = preprocessing.StandardScaler()
+    vectorizer = Vectorizer()
+    csp = CSP(n_components = int(inifile.get('setting', 'n_components')), reg=None, log=True, 
+            norm_trace=False, transform_into='average_power')
+    # (re)load the data to save memory
+    for path, event in path_b:
+        epochs.append(Epoch_raw.Epochs_raw(path, event, event_id, fmin, fmax, tmin, tmax))
+    epochs = concatenate_epochs(epochs)
+    labels = epochs.events[:, -1]
+    # remove evoked response
+    #epochs.subtract_evoked()
+    epochs_train = epochs.copy().crop(tmin=0.25, tmax=1.25)
+    epochs_data_train = epochs_train.get_data()
+    x = csp.fit_transform(epochs_data_train, labels)
+    csp_map.append(x)
+    del epochs
+
+print(csp_map)
--- epoch_raw.py
+++ epoch_raw.py
@@ -5,7 +5,8 @@ import pandas as pd
 
 class Epoch_raw:
     def __init__(self, path, event, event_id, fmin=2, fmax=45, tmin=-1, tmax=4,
-                channel_names = ["FCz", "FC1", "FC2", "FC3", "FC4", "Cz", "C1", "C2", "C3", "C4"]):
+                channel_names = ["FCz", "FC1", "FC2", "FC3", "FC4", "Cz", "C1", "C2", "C3", "C4"],
+                exclude_names = None):
         #mustparameter
         self.path = path
         self.event = event
@@ -17,13 +18,14 @@ class Epoch_raw:
         self.tmin = tmin
         self.tmax = tmax
         self.channel_names = channel_names
+        self.exclude_names = exclude_names
     
     def Epochs_raw(path, event, event_id, fmin = 2, fmax = 45, tmin = -1, tmax = 4, 
                     channel_names = ["FCz", "FC1", "FC2", "FC3", "FC4", "Cz", "C1", "C2", "C3", "C4"]):
         raw = read_raw_edf(path, stim_channel=False, preload=True)
         event = pd.read_csv(event, header=None)
         events = event.values
-        picks = mne.pick_channels(raw.info["ch_names"], channel_names)
+        picks = mne.pick_types(raw.info["ch_names"], eeg=True, stim=True, include=channel_names, exclude=None)
         raw.filter(fmin, fmax, n_jobs=1,  
                 l_trans_bandwidth=1,  
                 h_trans_bandwidth=1)
