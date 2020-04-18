50678a71f2dee9d9cecb5c0e645727090591243c
shaream <sprinter0208@outlook.jp>
2020-04-18 16:29:00 +0900

    2020/04/18

--- csp_size.py
+++ csp_size.py
@@ -47,7 +47,7 @@ path = inifile.get('setting', 'path')
 
 if path == "day":
     path_b = [(PATHfile.edfpath(name, day, "1"), PATHfile.eventpath(name, day, "1")),
-        (PATHfile.edfpath(name, day, "2"), PATHfile.eventpath(name, day, "2"))]
+        (PATHfile.edfpath(name, day, "2"), PATHfile.eventpath(name, day, "2")),
         (PATHfile.edfpath(name, day, "3"), PATHfile.eventpath(name, day, "3"))]
 elif path == "trial":
     path_b = [(PATHfile.edfpath(name, day, trial), PATHfile.eventpath(name, day, trial))]
